63f7a68454f71b7871923a845d86662020e47109
bloodysnow <bloodysnowx@gmail.com>
2016-09-19 13:43:27 +0900

    wip

--- HamelnReader/Network/HamelnClient.swift
+++ HamelnReader/Network/HamelnClient.swift
@@ -1,5 +1,7 @@
 import Foundation
 import Alamofire
+import RxAlamofire
+import RxSwift
 import Kanna
 
 class HamelnClient {
@@ -29,19 +31,33 @@ class HamelnClient {
         }
     }
     
-    static func originalWorks() {
-        // #main > div:nth-child(1) > form > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > select > option
-        let parameters: Parameters = [Param.Mode.rawValue: "search"]
-        Alamofire.request(URL.Base.getUrl(), parameters: parameters).responseString { (response) in
-            if let str = response.result.value { print(str) }
+    static func originalWorks() -> Observable<[OriginalWork]> {
+        let parameters: [String: AnyObject] = [Param.Mode.rawValue: "search" as AnyObject]
+        return requestString(.get, URL.Base.getUrl(), parameters: parameters).map { (response, str) -> [OriginalWork] in
+            guard let html = Kanna.HTML(html: str, encoding: .utf8) else { return [] }
+            let options = html.css("#main > div:nth-child(1) > form > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > select > option")
+            return options.flatMap({ (elem) -> OriginalWork? in
+                guard let value = elem["value"], let name = elem.innerHTML, value.characters.count > 0, name.characters.count > 0 else { return nil }
+                let work = OriginalWork()
+                work.value = value
+                work.name = name
+                return work
+            })
         }
     }
     
-    static func searchTypes() {
-        // #main > div:nth-child(1) > form > div > table > tbody > tr:nth-child(2) > td:nth-child(4) > select > option
-        let parameters: Parameters = [Param.Mode.rawValue: "search"]
-        Alamofire.request(URL.Base.getUrl(), parameters: parameters).responseString { (response) in
-            if let str = response.result.value { print(str) }
+    static func searchTypes() -> Observable<[SearchType]> {
+        let parameters: [String: AnyObject] = [Param.Mode.rawValue: "search" as AnyObject]
+        return requestString(.get, URL.Base.getUrl(), parameters: parameters).map { (response, str) -> [SearchType] in
+            guard let html = Kanna.HTML(html: str, encoding: .utf8) else { return [] }
+            let options = html.css("#main > div:nth-child(1) > form > div > table > tbody > tr:nth-child(2) > td:nth-child(4) > select > option")
+            return options.flatMap({ (elem) -> SearchType? in
+                guard let valueStr = elem["value"], let name = elem.innerHTML, valueStr.characters.count > 0, name.characters.count > 0, let value = Int(valueStr) else { return nil }
+                let type = SearchType()
+                type.value = value
+                type.name = name
+                return type
+            })
         }
     }
 }
